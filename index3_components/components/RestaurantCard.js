ca4fcf947ed641b30d224ea599b91f6705cb01a7
Himanshi Gupta <himanshigupta22101@gmail.com>
2025-02-10 12:45:34 +0530

    folder index3_components created

---
+++ index3_components/components/RestaurantCard.js
@@ -0,0 +1,29 @@
+// const RestaurantCard = (props) => {
+//     const {resName, cuisine} = props;
+//     return (
+//       <div className="res-card" style={styleCard}>
+//         <img className="food" src="https://images.immediate.co.uk/production/volatile/sites/30/2022/08/Corndogs-7832ef6.jpg?quality=90&resize=556,505"/>
+//         <h3>{resName}</h3>
+//         <h4>{props.cuisine}</h4>
+//       </div>
+//     )
+// }
+
+import { CDN_LINK } from "../utils/constants";
+
+const styleCard = {
+    backgroundColor: "bisque"
+}
+
+const RestaurantCard = (props) => {
+    const {resData} = props;
+    const {name, cuisines, cloudinaryImageId} = resData?.info;
+    return (
+      <div className="res-card" style={styleCard}>
+        <img className="food" src={CDN_LINK + cloudinaryImageId}/>
+        <h3>{name}</h3>
+        <h4>{cuisines[0]}</h4>
+      </div>
+    )
+}
+export default RestaurantCard;
---
+++ index3_components/utils/constants.js
@@ -0,0 +1,3 @@
+
+export const CDN_LINK = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const LOGO_URL = "https://marketplace.canva.com/EAGN6tjw2w8/1/0/1600w/canva-orange-and-yellow-kitchen-food-logo-2G4Q5SFCJ28.jpg";
